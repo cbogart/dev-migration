f33c957cf2c3914b3f0c35c51b0498aa7d717fac
Chris Bogart <cbogartdenver@gmail.com>
2018-05-04 18:05:24 -0400

    Initial commit

---
+++ scripts/authactPypi.sh
@@ -0,0 +1,8 @@
+
+for prinfo in `gunzip -c data/fromOSCAR/prjlist.Pypi.p2c.gz`
+do
+   PROJECT=`echo $prinfo | awk -F\; '{print $1}'`
+   COMMITS=`echo $prinfo | cut -d\; -f3- | perl -ane 's/\;/\n/g;print'`
+   WHENFO=`echo "$COMMITS" | /da3_data/lookup/showCmt.perl 0`
+   echo "$WHENFO" | awk -F\; -v proj=$PROJECT -v eco=Pypi '{print proj "," eco "," $4 "," $6}'
+done
---
+++ scripts/list_author_activities.sh
@@ -0,0 +1,2 @@
+bash scripts/authactNPM.sh > data/fromOSCAR/authact.NPM.csv.gz
+bash scripts/authactPypi.sh > data/fromOSCAR/authact.Pypi.csv.gz
