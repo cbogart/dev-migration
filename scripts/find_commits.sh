4da0f6343d9c9238a3219652ce73228fb3d245c5
Chris Bogart <cbogartdenver@gmail.com>
2018-07-11 16:50:11 -0400

    Removed some old files

--- scripts/find_commits.sh
+++
@@ -1,5 +0,0 @@
-
-for eco in Atom Bioconductor Cargo CocoaPods CPAN CRAN Go Hackage Hex Lua Maven NPM NuGet Packagist Pypi Rubygems Stackage
-do
-    cat data/fromLibrariesIo/${eco}-repos.txt | /da3_data/lookup/Prj2CmtShow.perl /da4_data/basemaps/Prj2CmtG 1 8 | gzip > data/fromOSCAR/prjlist.${eco}.p2c.b.gz
-done
