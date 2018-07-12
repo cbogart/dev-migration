985d02274c2cae88ad6ffaa01b1ada30d257e35d
Chris Bogart <cbogartdenver@gmail.com>
2018-07-12 10:48:20 -0400

    print to track progress

--- scripts/06_extract-specific-influence.py
+++ scripts/06_extract-specific-influence.py
@@ -5,15 +5,18 @@ from collections import defaultdict
 infl = dill.load(open("/data/play/cbogart/infl.dill"))
 fromeco = sys.argv[1]
 toeco = sys.argv[2]
+print "Loaded influence data"
 
 pkgs = defaultdict(lambda: defaultdict(int))
 inf = infl[fromeco][toeco]
 years = set()
 for pkg in inf:
+    print pkg
     for yr in inf[pkg]:
         pkgs[pkg][yr] += inf[pkg][yr]
         years.add(yr)
 
+print "Writing"
 import csv
 ou = csv.writer(open("/data/play/cbogart/infl_pkg.csv","w"))
 ou.writerow("year,package,influence".split(","))
